4a5e49c02b5afd479fb0e46d4b119c09888d1695
Veronica Perez <veperez@live.com>
2023-11-09 11:20:21 -0600

    boilerplate

--- server.js
+++ server.js
@@ -1,29 +1,47 @@
-const express = require("express");
-const path = require("path");
-const app = express();
-const routes = require("./routes");
-const sequelize = require("./config/connection");
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
+
+const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static("public"));
+// Set up sessions with cookies
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    // Stored in milliseconds
+    maxAge: 24 * 60 * 60 * 1000, // expires after 1 day
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-//home page route
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/index.html"))
-})
-//all other routes are directed elsewhere
-app.use("*", routes);
+app.use(routes);
 
-
-const okToSync = (process.env.NODE_ENV === "production") ? false: true;
-sequelize.sync({ force: okToSync}).then(() => {
-    app.listen(PORT, () => console.log("Now listening"));
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () =>
+    console.log(
+      `\nServer running on port ${PORT}. Visit http://localhost:${PORT} and create an account!`
+    )
+  );
 });
-
-
-
-
-
