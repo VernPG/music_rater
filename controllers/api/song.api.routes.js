95351ec48d2c0858128880381402f3342ea897e4
Arthur Henningfield <144608814+kylatae@users.noreply.github.com>
2023-11-13 10:14:30 -0600

    API Skeleton in working order

--- controllers/api/song.api.routes.js
+++ controllers/api/song.api.routes.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
-const User = require("../../models/User");
+const Song = require("../../models/Song");
 
 //get all records
 router.get("/", async (req, res) => {
   try {
-    const payload = await User.findAll();
+    const payload = await Song.findAll();
     res.status(200).json({ status: "success", payload });
   } catch (err) {
     res.status(500).json({ status: "error", payload: err.message });
@@ -13,7 +13,7 @@ router.get("/", async (req, res) => {
 //get one record by pk
 router.get("/:id", async (req, res) => {
   try {
-    const payload = await User.findByPk(req.params.id);
+    const payload = await Song.findByPk(req.params.id);
     res.status(200).json({ status: "success", payload });
   } catch (err) {
     res.status(500).json({ status: "error", payload: err.message });
@@ -22,7 +22,7 @@ router.get("/:id", async (req, res) => {
 //create a new record
 router.post("/", async (req, res) => {
   try {
-    const payload = await User.create(req.body);
+    const payload = await Song.create(req.body);
     res.status(200).json({ status: "success", payload });
   } catch (err) {
     res.status(500).json({ status: "error", payload: err.message });
@@ -31,7 +31,7 @@ router.post("/", async (req, res) => {
 //update a record
 router.put("/:id", async (req, res) => {
   try {
-    const payload = await User.update(req.body, {
+    const payload = await Song.update(req.body, {
       where: {
         id: req.params.id,
       },
@@ -44,7 +44,7 @@ router.put("/:id", async (req, res) => {
 
 router.delete("/:id", async (req, res) => {
   try {
-    const payload = await User.destroy({
+    const payload = await Song.destroy({
       where: {
         id: req.params.id,
       },
@@ -55,23 +55,5 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
-// const regenToken = async()=>{
-//   if(!token){
-//       return;
-//   }
-//   console.log(token);
-//   const mainTokenExp = api.decode(token).exp;
-//   if(mainTokenExp <= Date.now()/1000){
-//       //main token has expired
-//       const newToken = await axios.post("http://localhost:5000/api/user/token", {
-//           token:reToken,
-//       })
-//       token = newToken.data.accessToken;
-//       console.log(token)
-//       localStorage.setItem("accessToken", token);
-//   }
-// }
-
-// let intrId = setInterval(regenToken, 55(60*1000));
 
 module.exports = router;
